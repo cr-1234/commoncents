5cb9d935e45106eeaf3b5a5b9c3958d137bccf7c
cr-1234 <ebayrocks2017@gmail.com>
2026-06-01 16:37:45 -0400

    Add new deal for Multi-Function Screw Extractor Pliers

--- data.js
+++ data.js
@@ -24,3 +24,13 @@ const categories = [
 ============================================================================== */
 
 const deals = [];
+"{
+    id: 1,
+    title: "Multi-Function Screw Extractor Pliers",
+    category: ""Household"",
+    price: 13.99,
+    original_price: 15,
+    image: ""https://m.media-amazon.com/images/I/71QHf6Xjy1L._AC_SX679_.jpg"",
+    link: ""https://amzn.to/4uN9Lns"",
+    featured: true
+},"
