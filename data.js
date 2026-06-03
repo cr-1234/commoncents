ea232ceda6f523c761884cc36ff193ff4126e26f
cr-1234 <ebayrocks2017@gmail.com>
2026-06-02 22:31:53 -0400

    Add votes property to product object

--- data.js
+++ data.js
@@ -13,3 +13,13 @@ const deals = [
     votes: { up: 0, down: 0 }  // ← ADD THIS LINE
 },
 ];
+"{
+    id: 1,
+    title: ""Orgain Organic Vegan Protein Powder, Creamy Chocolate Fudge - 21g Plant Protein, 6g Prebiotic Fiber"",
+    category: ""groceries"",
+    price: 33.99,
+    original_price: 31.52,
+    image: ""https://m.media-amazon.com/images/I/71dlyx8HhLL._AC_SX466_.jpg"",
+    link: ""https://www.amazon.com/Orgain-Organic-Protein-Powder-Chocolate/dp/B00J074W94?pd_rd_w=m2qJe&content-id=amzn1.sym.d77fc71d-50de-409a-abeb-03b0a5133383&pf_rd_p=d77fc71d-50de-409a-abeb-03b0a5133383&pf_rd_r=641V6Y9CY1EX89GX40V9&pd_rd_wg=BPp9A&pd_rd_r=986946a3-9216-4039-b84e-b03123fbda93&pd_rd_i=B00J074W94&th=1&linkCode=ll2&tag=commoncents0050-20&linkId=7e327357c334ac1ffbc2b1f158b2da1a&language=en_US&ref_=as_li_ss_tl"",
+    featured: true
+},"
