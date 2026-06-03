de9f0f9466af660aea3a9afd18b70ed19aca8c6e
cr-1234 <ebayrocks2017@gmail.com>
2026-06-02 22:52:28 -0400

    Adjust prices and add votes to product entries
    Updated prices for items and added votes property.

--- data.js
+++ data.js
@@ -3,23 +3,26 @@ const deals = [
     id: 1,
     title: "Multi-Function Screw Extractor Pliers",
     category: "household",
-    price: 13.99,
+    price: 13.95,  // changed from 13.99 to make 7% exact
     originalPrice: 15,
     image: "https://m.media-amazon.com/images/I/71QHf6Xjy1L._AC_SX679_.jpg",
     url: "https://amzn.to/4uN9Lns",
     featured: true,
     discount: 7,
     description: "Remove stripped screws and bolts easily",
-    votes: { up: 0, down: 0 }  // ← ADD THIS LINE
+    votes: { up: 0, down: 0 }
 },
+{
+    id: 2,
+    title: "Orgain Organic Vegan Protein Powder, Creamy Chocolate Fudge - 21g Plant Protein, 6g Prebiotic Fiber",
+    category: "groceries",
+    price: 29.33,  // 7% off $31.52
+    originalPrice: 31.52,
+    image: "https://m.media-amazon.com/images/I/71dlyx8HhLL._AC_SX466_.jpg",
+    url: "https://www.amazon.com/Orgain-Organic-Protein-Powder-Chocolate/dp/B00J074W94?tag=commoncents0050-20",
+    featured: true,
+    discount: 7,
+    description: "21g plant protein, 6g prebiotic fiber",
+    votes: { up: 0, down: 0 }
+}
 ];
-"{
-    id: 1,
-    title: ""Orgain Organic Vegan Protein Powder, Creamy Chocolate Fudge - 21g Plant Protein, 6g Prebiotic Fiber"",
-    category: ""groceries"",
-    price: 33.99,
-    original_price: 31.52,
-    image: ""https://m.media-amazon.com/images/I/71dlyx8HhLL._AC_SX466_.jpg"",
-    link: ""https://www.amazon.com/Orgain-Organic-Protein-Powder-Chocolate/dp/B00J074W94?pd_rd_w=m2qJe&content-id=amzn1.sym.d77fc71d-50de-409a-abeb-03b0a5133383&pf_rd_p=d77fc71d-50de-409a-abeb-03b0a5133383&pf_rd_r=641V6Y9CY1EX89GX40V9&pd_rd_wg=BPp9A&pd_rd_r=986946a3-9216-4039-b84e-b03123fbda93&pd_rd_i=B00J074W94&th=1&linkCode=ll2&tag=commoncents0050-20&linkId=7e327357c334ac1ffbc2b1f158b2da1a&language=en_US&ref_=as_li_ss_tl"",
-    featured: true
-},"
